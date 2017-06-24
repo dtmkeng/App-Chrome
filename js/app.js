150115e0df6424aa3ffecf2e35ae410ac12f16d1
dtmkeng <kang081@hotmail.com>
2017-06-25 00:29:21 +0700

    add game

--- js/app.js
+++ js/app.js
@@ -1,8 +1,8 @@
 chrome.app.runtime.onLaunched.addListener(function() {
   chrome.app.window.create('index.html', {
     'bounds': {
-      'width': 500,
-      'height': 500
+      'width': 480,
+      'height': 320
     }
   });
 });
